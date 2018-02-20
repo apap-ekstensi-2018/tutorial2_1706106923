07a22b672c21b9273996e7ef8c6b73b959c62014
rahma <rahma.putrianasaricoc@gmail.com>
2018-02-20 23:37:32 +0700

    commit project

---
+++ src/main/java/com/example/demo/Tutorial21706106923Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial21706106923Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial21706106923Application.class, args);
+	}
+}
