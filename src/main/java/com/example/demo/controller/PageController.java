7f2fc512c5a0ea52fd0d1b1d2151f42647c4c9a5
rahma <rahma.putrianasaricoc@gmail.com>
2018-02-21 10:30:13 +0700

    commit greeting RequestParam try1

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,5 +1,25 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class PageController {
+	
+		@RequestMapping("/hello")
+		public String index()
+		{
+			return "hello";
+		}
+		
+		@RequestMapping("/greeting")
+		public String greeting(@RequestParam(value="name") String name, Model model)
+		{
+			model.addAttribute("name",name);
+			return "greeting";
+		}
+
 
 }
